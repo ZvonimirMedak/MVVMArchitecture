bc489142f03d2664c12000e4ea537087dc4dc943
Zvonimir Medak <zvonimir.medak@hotmail.com>
2021-04-24 21:23:09 +0200

    Initial Commit

---
+++ MVVMArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMArchitecture
+//
+//  Created by Zvonimir Medak on 24.04.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
